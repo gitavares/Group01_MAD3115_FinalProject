65a395db26dc8b98934f772949c68b03d7cb86f9
Giselle Tavares <gitavares@hotmail.com>
2018-11-03 00:47:17 -0400

    firebase config and files

---
+++ FinalProject/FinalProject/RegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterViewController.swift
+//  FinalProject
+//
+//  Created by Giselle Tavares on 02/11/18.
+//  Copyright © 2018 Giselle Tavares. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
