011f897ffaa7fafbbef7ef2f87d8786aa7313c99
merendaz <merendaz@gmail.com>
2018-11-13 20:09:55 -0500

    Create View, do the Outlets, TimeStamp, Navigation.

---
+++ FinalProject/FinalProject/Menu/AddTicketViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AddTicketViewController.swift
+//  FinalProject
+//
+//  Created by Antonio Merendaz do Carmo Nt on 2018-11-13.
+//  Copyright © 2018 Giselle Tavares. All rights reserved.
+//
+
+import UIKit
+
+class AddTicketViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+//        lblTimeStamp.text = "\(Date().currentDateTime)"
+        self.title = "Add Ticket"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
